c14ffb600aae76ec14c18f70c65ceb771ef09a80
Kauan Batista ADS-B <kauan18br@gmail.com>
2025-06-15 19:54:19 -0300

    logo e alguns detalhes

--- app/src/main/java/com/unasp/poupae/view/MainActivity.kt
+++ app/src/main/java/com/unasp/poupae/view/MainActivity.kt
@@ -3,19 +3,63 @@ package com.unasp.poupae.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import com.unasp.poupae.R
+import android.content.Intent
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import androidx.appcompat.widget.Toolbar
 import com.unasp.poupae.dialog.AddTransactionDialog
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.unasp.poupae.view.fragments.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        // Setup Drawer
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+        toolbar = findViewById(R.id.toolbar)
 
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_config -> {
+                    Toast.makeText(this, "Abrir configurações", Toast.LENGTH_SHORT).show()
+                    // abrir tela de configurações futuramente
+                    true
+                }
+                R.id.nav_logout -> {
+                    FirebaseAuth.getInstance().signOut()
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    finish()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        // Navegação inferior
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigation)
         bottomNav.setOnItemSelectedListener { item ->
             if (item.itemId == R.id.menu_add) {
                 AddTransactionDialog().show(supportFragmentManager, "addDialog")
@@ -37,6 +81,6 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
-        bottomNav.selectedItemId = R.id.menu_home // default
+        bottomNav.selectedItemId = R.id.menu_home
     }
 }
